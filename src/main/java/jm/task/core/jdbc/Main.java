1fc683f15070fc447b9fe3ec4497ec9327bed384
Dmitrii Kulakov <koolda@yandex.ru>
2023-04-26 20:50:12 +0300

    Second day starts

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,8 +2,11 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 
 import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    Connection connection;
 
+    public static void getConnection() {
+
+        final String URL = "jdbc:mysql://localhost:3306/mybase";
+        final String USERNAME = "rootroot";
+        final String PASSWORD = "root";
+        Connection connection;
+
+        try {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("ok");
+            }
+            connection.close();
+            if (connection.isClosed()) {
+                System.out.println("close");
+            }
+        } catch (SQLException e) {
+            System.err.println("Не удалось загрузить драйвер");
+//            e.printStackTrace();
+
+        }
+
+    }
 }
