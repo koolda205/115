41e8f11d319dca3557410c89ded8848a21ac54bb
Dmitrii Kulakov <koolda@yandex.ru>
2023-04-26 21:43:14 +0300

    Connection ok (try-catch without connection.close();)

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,27 +12,25 @@ public class Util {
 
     public static void getConnection() {
 
-        final String URL = "jdbc:mysql://localhost:3306/mybase";
+        final String URL = "jdbc:mysql://localhost:3306/mydbtest";
         final String USERNAME = "rootroot";
         final String PASSWORD = "root";
-        Connection connection;
+
+        Connection connection = null;
 
         try {
             Driver driver = new FabricMySQLDriver();
             DriverManager.registerDriver(driver);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             if (!connection.isClosed()) {
-                System.out.println("ok");
+                System.out.println("connection ok");
             }
-            connection.close();
-            if (connection.isClosed()) {
-                System.out.println("close");
-            }
+//                connection.close();
+//            if (connection.isClosed()) {
+//                System.out.println("connection close");
+//            }
         } catch (SQLException e) {
             System.err.println("Не удалось загрузить драйвер");
-//            e.printStackTrace();
-
         }
-
     }
 }
